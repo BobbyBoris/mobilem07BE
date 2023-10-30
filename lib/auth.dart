70e39e5beb46e3645f10ec97be063a48ef0fe161
bobby <114061942+BobbyBoris@users.noreply.github.com>
2023-10-30 14:45:18 +0700

    Update auth.dart

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,25 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthFirebase {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<String?> signUp(String email, String password) async {
+    UserCredential authResult = await _firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
+    User? user = authResult.user;
+    return user?.uid;
+  }
 
   Future<String?> login(String email, String password) async {
     try {
       UserCredential authResult = await _firebaseAuth
           .signInWithEmailAndPassword(email: email, password: password);
-      User? user = authResult.user;
-      return user?.uid;
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
-  }
-
-  Future<String?> signUp(String email, String password) async {
-    try {
-      UserCredential authResult = await _firebaseAuth
-          .createUserWithEmailAndPassword(email: email, password: password);
       User? user = authResult.user;
       return user?.uid;
     } catch (e) {
