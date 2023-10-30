8fd6812a840843f6bf9016c2064225e96ba3eb56
bobby <114061942+BobbyBoris@users.noreply.github.com>
2023-10-30 14:45:32 +0700

    Update loginScreen.dart

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:minggu7/auth.dart';
+import 'package:minggu7/home.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key});
+  const LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -15,20 +16,14 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   void initState() {
-    super.initState();
     auth = AuthFirebase();
     auth.getUser().then((value) {
+      MaterialPageRoute route;
       if (value != null) {
-        navigateToHome(value.uid);
+        route = MaterialPageRoute(builder: (context) => MyHome(wid: value.uid));
+        Navigator.pushReplacement(context, route);
       }
     }).catchError((err) => print(err));
-  }
-
-  void navigateToHome(String uid) {
-    MaterialPageRoute route = MaterialPageRoute(
-      builder: (context) => MyHome(email: "example@example.com", wid: uid),
-    );
-    Navigator.pushReplacement(context, route);
   }
 
   @override
@@ -36,113 +31,66 @@ class _LoginScreenState extends State<LoginScreen> {
     return FlutterLogin(
       onLogin: _loginUser,
       onRecoverPassword: _recoverPassword,
-      onSignup: _onSignup,
+      onSignup: _onSignUp,
       passwordValidator: (value) {
         if (value != null) {
           if (value.length < 6) {
-            return "Password Must be 6 Characters";
+            return "Password Must Be 6 Characters";
           }
         }
       },
       loginProviders: <LoginProvider>[
         LoginProvider(
+            callback: _onLoginGoogle,
             icon: FontAwesomeIcons.google,
-            label: 'Google',
-            callback: _onLoginGoogle),
+            label: 'Google')
       ],
       onSubmitAnimationCompleted: () {
         auth.getUser().then((value) {
+          MaterialPageRoute route;
           if (value != null) {
-            navigateToHome(value.uid);
+            route =
+                MaterialPageRoute(builder: (context) => MyHome(wid: value.uid));
           } else {
-            MaterialPageRoute route =
-                MaterialPageRoute(builder: (context) => LoginScreen());
-            Navigator.pushReplacement(context, route);
+            route = MaterialPageRoute(builder: (context) => LoginScreen());
           }
+          Navigator.pushReplacement(context, route);
         }).catchError((err) => print(err));
       },
     );
   }
 
-  Future<String?> _loginUser(LoginData data) {
+  Future<String?>? _loginUser(LoginData data) {
     return auth.login(data.name, data.password).then((value) {
       if (value != null) {
-        MyHome myHome = MyHome(wid: value, email: data.name);
-        Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => myHome));
+        MaterialPageRoute(builder: (context) => MyHome(wid: value));
       } else {
         final snackbar = SnackBar(
-          content: const Text('Login Failed, user not found'),
-          action: SnackBarAction(
-            label: 'OK',
-            onPressed: () {
-              // Tindakan jika login gagal
-            },
-          ),
+          content: const Text('Login Failed, user not Found'),
+          action: SnackBarAction(label: 'OK', onPressed: () {}),
         );
         ScaffoldMessenger.of(context).showSnackBar(snackbar);
-        Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => LoginScreen()));
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const LoginScreen()));
       }
     });
   }
 
-  Future<String>? _recoverPassword(String name) {
-    // Tambahkan logika pemulihan kata sandi di sini jika diperlukan
+  Future<String?>? _recoverPassword(String name) {
     return null;
   }
 
-  Future<String?> _onSignup(SignupData data) {
+  Future<String?>? _onSignUp(SignupData data) {
     return auth.signUp(data.name!, data.password!).then((value) {
-      if (value != null) {
-        final snackbar = SnackBar(
-          content: const Text('Sign up Successfull'),
-          action: SnackBarAction(
-            label: 'OK',
-            onPressed: () {},
-          ),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackbar);
-      }
+      final snackbar = SnackBar(
+        content: const Text("Sign Up Successful"),
+        action: SnackBarAction(label: 'OK', onPressed: () {}),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
     });
   }
 
-  Future<String?> _onLoginGoogle() async {
-    // Tambahkan logika login menggunakan Google di sini jika diperlukan
+  Future<String?>? _onLoginGoogle() {
     return null;
-  }
-}
-
-class MyHome extends StatelessWidget {
-  final String email;
-  final String wid;
-
-  MyHome({required this.email, required this.wid});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout_sharp),
-            tooltip: 'Logout',
-            onPressed: () {
-              // Tambahkan kode untuk logout di sini
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Welcome $email"),
-            Text("ID $wid"),
-          ],
-        ),
-      ),
-    );
   }
 }
